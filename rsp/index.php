fd1c3809ff50bbcfe6e3d73187e3342f272648a8
Alexander Levashov <alevash1@gmail.com>
2017-08-01 00:03:02 +0300

    Камень-Ножницы-Бумага v.0.2
    Добавлены "жизни" для игрока и компьютера. Привязка к сессии.

--- rsp/index.php
+++ rsp/index.php
@@ -1,23 +1,67 @@
 <?php
-	function fight() 
-	{
-		$me = $_POST['udar'];
-		
-		$enemy = mt_rand(1, 3);
-		
-		if ($me == '1' && $enemy == 1) echo "Камень против камня - ничья!";
-		elseif ($me == '2' && $enemy == 2) echo "Ножницы против ножниц - ничья!";
-		elseif ($me == '3' && $enemy == 3) echo "Бумага против бумаги - ничья!";
-		elseif ($me == '1' && $enemy == 2) echo "Камень против ножниц - вы победили!";
-		elseif ($me == '1' && $enemy == 3) echo "Камень против бумаги - вы проиграли!";
-		elseif ($me == '2' && $enemy == 1) echo "Ножницы против камня - вы проиграли!";
-		elseif ($me == '2' && $enemy == 3) echo "Ножницы против бумаги - вы победили!";
-		elseif ($me == '3' && $enemy == 1) echo "Бумага против камня - вы победили!";
-		elseif ($me == '3' && $enemy == 2) echo "Бумага против ножниц - вы проиграли!";
-		else echo "Попробуйте ещё!";
-	}
+    session_start();
+
+    $me = $_POST['udar'] ?? false;
+    $enemy = mt_rand(1, 3);
+
+    !empty($_SESSION['result1']) ?: $_SESSION['result1'] = 10;
+    !empty($_SESSION['result2']) ?: $_SESSION['result2'] = 10;
+
+    if (isset($_REQUEST['fight'])) {
+        if ($me == '1' && $enemy == 1) {
+            $result = "Камень против камня - ничья!";
+            $me_life = $_SESSION['result1'];
+            $enemy_life = $_SESSION['result2'];
+        }
+        elseif ($me == '2' && $enemy == 2) {
+            $result = "Ножницы против ножниц - ничья!";
+            $me_life = $_SESSION['result1'];
+            $enemy_life = $_SESSION['result2'];
+        }
+        elseif ($me == '3' && $enemy == 3) {
+            $result = "Бумага против бумаги - ничья!";
+            $me_life = $_SESSION['result1'];
+            $enemy_life = $_SESSION['result2'];
+        }
+        elseif ($me == '1' && $enemy == 2) {
+            $result = "Камень против ножниц - вы победили!";
+            $enemy_life = --$_SESSION['result2'];
+            $me_life = $_SESSION['result1'];
+        }
+        elseif ($me == '1' && $enemy == 3) {
+            $result = "Камень против бумаги - вы проиграли!";
+            $me_life = --$_SESSION['result1'];
+            $enemy_life = $_SESSION['result2'];
+        }
+        elseif ($me == '2' && $enemy == 1) {
+            $result = "Ножницы против камня - вы проиграли!";
+            $me_life = --$_SESSION['result1'];
+            $enemy_life = $_SESSION['result2'];
+        }
+        elseif ($me == '2' && $enemy == 3) {
+            $result = "Ножницы против бумаги - вы победили!";
+            $me_life = $_SESSION['result1'];
+            $enemy_life = --$_SESSION['result2'];
+        }
+        elseif ($me == '3' && $enemy == 1) {
+            $result = "Бумага против камня - вы победили!";
+            $me_life = $_SESSION['result1'];
+            $enemy_life = --$_SESSION['result2'];
+        }
+        elseif ($me == '3' && $enemy == 2) {
+            $result = "Бумага против ножниц - вы проиграли!";
+            $me_life = --$_SESSION['result1'];
+            $enemy_life = $_SESSION['result2'];
+        }
+
+    }
+    else {
+        $me_life = 10;
+        $enemy_life = 10;
+    }
+
 ?>
-<form name="fight" action="" method="post">
+<form name="fight" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
 	<label>
 	Камень: <input type="radio" name="udar" value="1"><br>
 	Ножницы: <input type="radio" name="udar" value="2" ><br>
@@ -25,6 +69,8 @@
 	</label>
 	<input type="submit" name="fight" value="Погнали!">
 </form>
+<p>Ваши жизни: <?=$me_life?></p>
+<p>Жизни врага: <?=$enemy_life?></p>
 <?php
-	if($_REQUEST['fight']) fight();
+	if($_REQUEST['fight']) echo $result;
 ?>
